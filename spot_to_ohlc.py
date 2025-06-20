e0914d9c0b927376dda1c79daa92522973ce14e9
vikramsingh117 <vikramandanshu@gmail.com>
2025-06-20 15:29:34 +0530

    basic features created

--- spot_to_ohlc.py
+++ spot_to_ohlc.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 def main():
     df = pd.read_csv('SPOT_DATA.csv')
@@ -12,5 +13,22 @@ def main():
 
     ohlc = ohlc.dropna(how='all')
 
-    ohlc.to_csv('SPOT_OHLC.csv')
-    print('Saved daily OHLC to SPOT_OHLC.csv')
+    # Feature current day return
+    ohlc['Return'] = (ohlc['Close'] - ohlc['Open']) / ohlc['Open']
+    # Feature two: current high and past 3-day rolling hig
+    ohlc['Past3DayHigh'] = ohlc['High'].shift(1).rolling(window=3).max()
+    ohlc['High_vs_Past3High'] = ohlc['High'] - ohlc['Past3DayHigh']
+
+    # Feature 5: Next day  returns
+    ohlc['Next1DayReturn'] = ohlc['Close'].shift(-1) / ohlc['Close'] - 1
+    ohlc['Next2DayReturn'] = ohlc['Close'].shift(-2) / ohlc['Close'] - 1
+
+
+
+
+
+    ohlc.to_csv('SPOT_OHLC_FEATURES.csv')
+    print('Saved daily OHLC with features to SPOT_OHLC_FEATURES.csv')
+
+if __name__ == '__main__':
+    main()
